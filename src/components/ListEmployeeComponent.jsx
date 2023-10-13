23a41492580fe568a0fc8b73b09e7a35040e816c
baydev <baydev2002@gmail.com>
2023-10-13 20:06:32 +0300

    frontend commit

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -24,9 +24,8 @@ class ListEmployeeComponent extends Component {
                         <thead>
                             <tr>
                                 <th>Employee First Name</th>
-                                <th>Employee First Name</th>
-                                <th>Employee First Name</th>
-                                <th>Employee First Name</th>
+                                <th>Employee Last Name</th>
+                                <th>Employee Email</th>
                             </tr>
                         </thead>
                         <tbody>
